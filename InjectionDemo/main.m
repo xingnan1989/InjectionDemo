4a53bb1a51631ad69f9bbc1ec4ccae5aeeab95e0
xingnan1989 <179350761@qq.com>
2018-08-11 18:35:42 +0800

    Initial Commit

---
+++ InjectionDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  InjectionDemo
+//
+//  Created by lionking on 2018/8/11.
+//  Copyright © 2018年 lionKing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
