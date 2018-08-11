520c2b0fcfb65116623a52012a2ed55955203f53
xingnan1989 <179350761@qq.com>
2018-08-11 18:43:38 +0800

    First

--- InjectionDemo/ViewController.m
+++ InjectionDemo/ViewController.m
@@ -19,11 +19,10 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)injected
+{
+    NSLog(@"I've been injected: %@", self);
+    self.view.backgroundColor = [UIColor redColor];
 }
-
 
 @end
